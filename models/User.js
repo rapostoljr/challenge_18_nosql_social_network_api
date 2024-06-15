dbd23406991f99aae639ad3a3732bae1635adb85
RApostolJR <romuloapostol@gmail.com>
2024-06-14 23:27:33 -0700

    added server.js, fixed package.json to have server.js as main, added code for Reaction.js, added code for Thought.js, added code for User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,47 @@
 const { Schema, model, Types } = require('mongoose'); 
+
+const userSchema = newSchema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(email) {
+                    return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email);
+                }
+            }
+        },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+)
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('User',userSchema)
+module.exports = User;
