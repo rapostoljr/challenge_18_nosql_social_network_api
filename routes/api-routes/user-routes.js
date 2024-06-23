5166c115e514c9fe09fbc5f2b585299ffeffc94b
RApostolJR <romuloapostol@gmail.com>
2024-06-22 20:49:49 -0700

    made route for users and thought plural, added code for user-routes.js

--- routes/api-routes/user-routes.js
+++ routes/api-routes/user-routes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    deleteUser,
+    updateUser,
+} = require('../../controllers/user-controller.js');
+
+// /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+   .get(getOneUser)
+   .put(updateUser)
+   .delete(deleteUser);
+
 module.exports = router;
